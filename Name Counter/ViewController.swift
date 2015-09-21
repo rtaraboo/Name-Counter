d2b1ba9e5f8f122c218de866b23f9da4000a05ef
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-09-21 16:32:05 -0400

    Initial Commit

--- Name Counter/ViewController.swift
+++ Name Counter/ViewController.swift
@@ -8,8 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtTotalCharacters: UILabel!
+    
+    var nameData = ""
+    
+    
+    @IBAction func btnCalculate(sender: UIButton) {
+        
+        calculate()
+        
+    }
+    
+    
+    @IBAction func btnClear(sender: UIButton) {
+        
+        clear()
+        
+    }
+    
+    
+    @IBAction func txtNameACTION(sender: UITextField) {
+        
+        
+    }
+    
+    
+    func calculate(){
+        
+        nameData = txtName.text!
+        
+        
+        printCharaters()
+        
+        hideKeyboard()
+        
+        
+    }
+    
+    func clear() {
+        
+        txtName.text = ""
+        txtTotalCharacters.text = "0"
+        
+        hideKeyboard()
+        
+    }
+    
+    func printCharaters(){
+        
+        var nameArray = Array(nameData.characters)
+        
+     txtTotalCharacters.text = "\(nameArray.count)"
+        
+        
+    }
+    
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        hideKeyboard()
+    }
+    
+    func hideKeyboard(){
+        
+    txtName.resignFirstResponder()
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
